7fceff8492a31fb0cc195deb41b1aff83850f039
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2025-01-31 14:14:15 +0100

    added backend

---
+++ src/js/backend.mjs
@@ -0,0 +1,18 @@
+import PocketBase from "pocketbase";
+const pb = new PocketBase("http://127.0.0.1:8090");
+
+export async function getOffres() {
+    try {
+        let data = await pb.collection('Maison').getFullList({
+            sort: '-created',
+        });
+        data = data.map((item) => {
+            item.img = pb.files.getURL(item, item.imgUrl);
+            return item;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
+}
